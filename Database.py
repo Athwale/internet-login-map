ba67fb9cdf3d94375ac2f987aca463536e48cf79
Ondrej Mejzlik <omejzlik@redhat.com>
2020-04-05 16:19:22 +0200

    Link check

--- Database.py
+++ Database.py
@@ -40,8 +40,23 @@ class Database:
                             raise FormatError(self.DATABASE_ERROR + str(address) + ' is missing "' + str(char) + '"')
                     # Check attribute names
                     self._attribute_check(['password', 'question', 'linkto', 'notes'], values)
+                    # Check that linkto points to an existing record
+                    self._linkto_check(values, 'linkto')
 
-    def _attribute_check(self, valid_list, attr_dict):
+    def _linkto_check(self, attr_dict, kind: str) -> None:
+        """
+        Check that linkto from record attributes points to an existing record in database.
+        :param attr_dict: Record attributes from yaml. Like email data.
+        :param kind: str one of 'mail' and 'linkto'
+        :return: None
+        :exception FormatError if link does not lead to an existing record.
+        """
+        if attr_dict[kind]:
+            for link in attr_dict[kind]:
+                print(link)
+
+
+    def _attribute_check(self, valid_list, attr_dict) -> None:
         """
         Check that keys in attr_dict only have names listed in valid_list.
         :param valid_list: List of allowed string key names (attributes in yaml)
