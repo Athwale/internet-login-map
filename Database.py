15c24a4776725f4c06d3d1215d9b3883cbf0c112
Ondrej Mejzlik <omejzlik@redhat.com>
2020-04-05 11:12:37 +0200

    Attribute check

--- Database.py
+++ Database.py
@@ -34,8 +34,21 @@ class Database:
                 if len(mail.keys()) > 1:
                     raise FormatError(self.DATABASE_ERROR + str(mail) + ' record is malformed')
                 # Check mail format
-                for address in mail.keys():
+                for address, values in mail.items():
                     for char in ['@', '.']:
                         if char not in address:
                             raise FormatError(self.DATABASE_ERROR + str(address) + ' is missing "' + str(char) + '"')
+                    # Check attribute names
+                    self._attribute_check(['password', 'question', 'linkto', 'notes'], values)
 
+    def _attribute_check(self, valid_list, attr_dict):
+        """
+        Check that keys in attr_dict only have names listed in valid_list.
+        :param valid_list: List of allowed string key names (attributes in yaml)
+        :param attr_dict: Record attributes from yaml. Like email data.
+        :return: None
+        :exception FormatError if attributes do not match.
+        """
+        for attr in attr_dict.keys():
+            if attr not in valid_list:
+                raise FormatError(self.DATABASE_ERROR + 'invalid attribute "' + str(attr) + '"')
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # internet-login-map
 Website login database capable of drawing a graph of user's presence on the net.
+
+TODO:
+Checks:
+Check that linkto points to something existing in the database.
+Check that password is not empty.
